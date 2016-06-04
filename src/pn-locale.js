c0eeb136be487d7b948add99907e010cdf1eb883
mnguyen284 <mnguyen284@gmail.com>
2016-06-04 11:45:49 +0700

    1.0.0-rc1-final

---
+++ src/pn-locale.js
@@ -0,0 +1,35 @@
+(function () {
+
+    'use strict';
+
+    var module = angular.module('pnLocale', ['ngResource']);
+
+    module.directive('pnLocale', ['$resource',
+        function ($resource) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    var url = attrs.pnLocale;
+                    $resource(url).get(function (resources) {
+                        for (var name in resources) {
+                            var resource = resources[name];
+                            if (typeof resource === 'string' && resource.indexOf('{') !== -1) {
+                                resources['format' + name] = (function (resource) {
+                                    return function (params) {
+                                        var result = resource;
+                                        for (var paramKey in params) {
+                                            result = result.replace('{' + paramKey + '}', params[paramKey]);
+                                        }
+                                        return result;
+                                    };
+                                })(resource);
+                            }
+                        }
+                        scope.locale = resources;
+                    });
+                }
+            };
+        }
+    ]);
+
+})();
